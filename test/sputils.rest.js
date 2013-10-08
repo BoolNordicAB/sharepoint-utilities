a1f45775a0525b4dfbc3c0888fcbdb8edb99566f
Erik Kronberg <erik@variadic.me>
2013-10-08 15:08:21 +0200

    Adds get test for rest namespace

--- test/sputils.rest.js
+++ test/sputils.rest.js
@@ -3,4 +3,20 @@ describe('SharePoint REST API Wrapper', function () {
   it('should have a namespace', function () {
     expect(sputils).to.have.ownProperty('rest');
   });
+
+  describe('get', function () {
+    it('should have JSON/OData accept header', function (done) {
+      // Stub jQuery ajax
+      jQuery.ajax = function (config) {
+        expect(config)
+          .to.have.deep.property('headers.accept',
+                                 'application/json;odata=verbose');
+      };
+
+      sputils.rest.get("/")
+        .then(function (res) {
+          done();
+        });
+    });
+  });
 });
