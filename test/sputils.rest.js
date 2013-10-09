c09cc9cb6e9c22affefdeb2a040f305dee00cbde
Erik Kronberg <erik@variadic.me>
2013-10-09 15:30:57 +0200

    Adds testing of sputil rest post

--- test/sputils.rest.js
+++ test/sputils.rest.js
@@ -4,16 +4,48 @@ describe('SharePoint REST API Wrapper', function () {
     expect(sputils).to.have.ownProperty('rest');
   });
 
+  beforeEach(function () {
+    initialize_jquery();
+  });
+
   describe('get', function () {
-    it('should have JSON/OData accept header', function (done) {
-      // Stub jQuery ajax
+    it('should have correct config settings', function (done) {
+      // Mock jQuery ajax
       jQuery.ajax = function (config) {
         expect(config)
           .to.have.deep.property('headers.accept',
                                  'application/json;odata=verbose');
+        expect(config)
+          .to.have.property('url');
+        expect(config)
+          .to.have.property('type', 'GET');
       };
 
       sputils.rest.get("/")
+        .then(function (res) {
+          done();
+        });
+    });
+  });
+
+  describe('post', function () {
+    it('should have correct config settings', function (done) {
+
+      /* jshint evil: true */
+      document.write("<input id=__REQUESTDIGEST val=TestValue />");
+      /* jshint evil: false */
+
+      jQuery.ajax = function (config) {
+        expect(config)
+          .to.have.deep.property('headers.accept',
+                                 'application/json;odata=verbose');
+        expect(config)
+          .to.have.property('url');
+        expect(config)
+          .to.have.property('type', 'POST');
+      };
+
+      sputils.rest.post("/", {"test":"test"})
         .then(function (res) {
           done();
         });
