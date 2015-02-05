87ea9f0bd0f4bfabd2b2f21710e08c8b290a9c2d
Erik Kronberg <erik@variadic.me>
2015-02-05 09:45:51 +0100

    Updates Karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,8 +26,7 @@ module.exports = function (grunt) {
     },
     karma: {
       unit: {
-        configFile: 'karma.conf.js',
-        singleRun: true,
+        configFile: 'karma.conf.js'
       }
     },
     jshint: {
--- karma.conf.js
+++ karma.conf.js
@@ -1,55 +1,57 @@
 // Karma configuration
+// Generated on Thu Feb 05 2015 09:37:08 GMT+0100 (W. Europe Standard Time)
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(config) {
+  config.set({
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
-// list of files / patterns to load in the browser
-files = [
-  MOCHA,
-  MOCHA_ADAPTER,
-  'node_modules/chai/chai.js',
-  'bower_components/jquery/jquery.js',
-  'test/utilities/*.js',
-  'src/**/*.js',
-  'test/**/*.js'
-];
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha'],
 
-// list of files to exclude
-exclude = [];
+    // list of files / patterns to load in the browser
+    files: [
+      'node_modules/chai/chai.js',
+      'bower_components/jquery/jquery.js',
+      'test/utilities/*.js',
+      'src/**/*.js',
+      'test/**/*.js'
+    ],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+    // list of files to exclude
+    exclude: [
+    ],
 
-// web server port
-port = 8080;
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
 
-// cli runner port
-runnerPort = 9100;
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // web server port
+    port: 9876,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['IE'];
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['IE'],
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
