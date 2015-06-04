588b7823e0d79fa88fa0d13c4bb61fdcef0cf230
Björn Roberg <bjorn.roberg@gmail.com>
2015-06-04 16:31:38 +0200

    update docs

--- src/sputils.conversion.js
+++ src/sputils.conversion.js
@@ -2,16 +2,21 @@
 
 (function () {
   /**
-   * This is a description of the foo function.
-   * @param {string} str The string
-   * @returns {string}
-   */
-
-  var getUserNameFromClaim = function (string) {
-    var splitUserName = string.split("|");
+  Get the user name from the claims token string
+  @param {string} claimsString - the claims string
+  @returns {string} username
+  @example ```
+  var claimsToken = 'i:0ǵ.t|ipdomain|jdoe';
+  var username = sputils.getUserNameFromClaim(claimsToken);
+  console.log(username); // => 'jdoe'
+  ```
+  **/
+  var getUserNameFromClaim = function (claimsString) {
+    var splitUserName = claimsString.split("|");
     return splitUserName[splitUserName.length -1];
   };
 
+  /** @module sputils/conversion */
   sputils.conversion = {
     getUserNameFromClaim: getUserNameFromClaim
   };
