4c41a55403c67af67d122eb6de426ca9fd162951
Rebecka <rebeckamerkel@gmail.com>
2015-06-04 11:40:19 +0200

    Add getCurrentUser function in User module
    User utility getCurrentUser that returns a promise with the current SPUser object.

--- src/sputils.user.js
+++ src/sputils.user.js
@@ -10,6 +10,31 @@ function $_global_sputils_user () {
       window.location.href = "/_layouts/closeconnection.aspx";
     };
 
+    // Returns a promise with the current spuser object.
+    var getCurrentUser = function () {
+      var deferred = $.Deferred();
+
+      var clientContext = new SP.ClientContext.get_current();
+      var currentWeb = clientContext.get_web();
+      clientContext.load(currentWeb);
+      var currentUser = web.get_currentUser();
+
+      //Load currentUser to the context in order to retrieve the user data.
+      clientContext.load(currentUser);
+
+      var successHandler = function (currentUser) {
+        deferred.resolve(currentUser);
+      };
+
+      var errorHandler = function () {
+        deferred.reject('error');
+      };
+
+      clientContext.executeQueryAsync(successHandler, errorHandler);
+
+      return deferred.promise();
+    };
+
     window.sputils = window.sputils || {};
     window.sputils.user = {
       loginAsAnotherUser: loginAsAnotherUser,
