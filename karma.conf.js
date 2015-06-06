2bbf3b99aa0536a7796baeaaefbd9e4a099d9788
Björn Roberg <bjorn.roberg@gmail.com>
2015-06-06 22:36:16 +0200

    Add code coverage to karma runner
    don't forget to `npm install`
    
    Look at the coverage after doing `grunt` and start writing tests.

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,14 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'src/**/*.js': ['coverage']
+    },
 
     // web server port
     port: 9876,
@@ -47,6 +54,23 @@ module.exports = function(config) {
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
+
+    coverageReporter: {
+      // specify a common output directory
+      dir: 'coverage',
+      reporters: [
+        // reporters not supporting the `file` property
+        { type: 'html', subdir: 'report-html' },
+        //{ type: 'lcov', subdir: 'report-lcov' },
+        // reporters supporting the `file` property, use `subdir` to directly
+        // output them in the `dir` directory
+        //{ type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
+        //{ type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+        //{ type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
+        //{ type: 'text', subdir: '.', file: 'text.txt' },
+        //{ type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+      ]
+    },
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
