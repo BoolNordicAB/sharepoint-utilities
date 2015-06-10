0acc4b222a869a95e891875e7e8e2e69538d8155
Erik Kronberg <erik@variadic.me>
2015-06-10 16:45:48 +0200

    Add travis ci

--- karma.conf.js
+++ karma.conf.js
@@ -74,10 +74,10 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['IE'],
+    browsers: ['PhantomJS'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 };
